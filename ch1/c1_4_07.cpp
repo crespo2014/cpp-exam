ca77bd25e435430722139c6749e7bee540605a56
Lester <lester.crespo.es@gmail.com>
2015-02-22 15:16:51 +0000

    Added chapter 1 source code

---
+++ ch1/c1_4_07.cpp
@@ -0,0 +1,63 @@
+#include <list>
+#include <vector>
+#include <deque>
+#include <iostream>
+
+using namespace std;
+
+template <class  I>
+void print (const I & start, const I & end)
+{
+	I it;
+	for(it = start; it != end; ++it)
+	{
+		cout<< *it << " ";
+	}
+	cout<<endl;
+}
+
+int main()
+{
+	int a[] = {1,2,3,4,5,6,7,8,9,10};
+	//containers
+	vector<int> v(a,a+5);
+	deque<int> d(a,a+5);
+	list<int> l(a,a+5);
+
+	print(v.begin(), v.end());
+	print(d.begin(), d.end());
+	print(l.begin(), l.end());
+
+	cout<<"Assigning a new content:\n";
+	v.assign(a, a+10);
+	d.assign(a, a+10);
+	l.assign(a, a+10);
+	print(v.begin(), v.end());
+	print(d.begin(), d.end());
+	print(l.begin(), l.end());
+
+	cout<<"Assigning a new content II:\n";
+	v.assign(3, 100);
+	d.assign(3, 1000);
+	l.assign(3, 10000);
+	print(v.begin(), v.end());
+	print(d.begin(), d.end());
+	print(l.begin(), l.end());
+	return 0;
+}
+/*
+Console output:
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+Assigning a new content:
+1 2 3 4 5 6 7 8 9 10
+1 2 3 4 5 6 7 8 9 10
+1 2 3 4 5 6 7 8 9 10
+Assigning a new content II:
+100 100 100
+1000 1000 1000
+10000 10000 10000
+*/
+ 
+
---
+++ ch1/c1_4_13.cpp
@@ -0,0 +1,49 @@
+#include <deque>
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+template <class I>
+void print (const I & start, const I & end)
+{
+	for(I it = start; it != end; ++it)
+	{
+		cout<< *it << " ";
+	}
+	cout<<endl;
+}
+
+int main()
+{
+	int a[] = {1,2,3,4,5,6,7,8,9,10};
+	deque<int> d;
+	list<int> l;
+	
+	for(unsigned i = 0; i < 10; ++i)
+	{
+		d.push_front(i);
+		l.push_front(i);
+	}
+	cout<<"Deque:  ";	print(d.begin(), d.end());
+	cout<<"List:   ";	print(l.begin(), l.end());
+	
+	for(unsigned i = 0; i < 5; ++i)
+	{	
+		d.pop_front();
+		l.pop_front();
+	}
+	cout<<"Deque:  ";	print(d.begin(), d.end());
+	cout<<"List:   ";	print(l.begin(), l.end());
+
+	return 0;
+}
+/*
+Console output:
+Deque:  9 8 7 6 5 4 3 2 1 0
+List:   9 8 7 6 5 4 3 2 1 0
+Deque:  4 3 2 1 0
+List:   4 3 2 1 0
+*/
+ 
+
