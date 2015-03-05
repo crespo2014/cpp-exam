dd40dab7e81e0c1b8cf79709bf9aaa066cdc0ae9
Lester <lester.crespo.es@gmail.com>
2015-03-05 08:39:41 +0000

    Practice final assessment done

--- ch10/e10_05.cpp
+++ ch10/e10_05.cpp
@@ -1,8 +1,39 @@
+/*
+ *
+ Which of the following examples shows the proper way to create a new priority_queue container assuming all necessary declarations has been performed. Choose all that apply:
+ Select correct answers (multiple choice)
+ */
+#include <list>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <deque>
+#include <queue>
 
-Which of the following examples shows the proper way to create a new priority_queue container assuming all necessary declarations has been performed. Choose all that apply:
-Select correct answers (multiple choice)
-list_queue<int> l; priority_queue<int> q(l); ;;;;
-deque<int> d; priority_queue<int> q(d.begin(), d.end()); ;;;;
-queue<int> q; ;;;;
-deque<int> d; priority_queue<int> q(d);
-vector<int> v; priority_queue<int> q(v); ;;;;
+using namespace std;
+
+int main()
+{
+  {
+    list_queue<int> l;
+    priority_queue<int> q(l);
+    ;;;;
+  }
+  {
+    deque<int> d;
+    priority_queue<int> q(d.begin(), d.end());
+    ;;;;
+  }
+  {
+    queue<int> q;
+    ;;;;
+  }
+  {
+    deque<int> d;
+    priority_queue<int> q(d);
+  }
+  {
+    vector<int> v;
+    priority_queue<int> q(v);
+    ;;;;
+  }
