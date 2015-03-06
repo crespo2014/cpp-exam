5a6f40c107e187459d489fca0b779a00cec9f995
Lester <lester.crespo.es@gmail.com>
2015-03-06 00:31:32 +0000

    Chapter 11 Final review

--- ch11/e11_04.cpp
+++ ch11/e11_04.cpp
@@ -1,10 +1,33 @@
+/*
+ Which of the following examples show(s) the proper way to create a new queue container assuming all necessary declarations have been performed.
+ Choose all that apply:
+ Select correct answers (multiple choice)
+ */
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <list>
+using namespace std;
+int main()
+{
 
-
-Which of the following examples show(s) the proper way to create a new queue container assuming all necessary declarations have been performed.
-Choose all that apply:
-Select correct answers (multiple choice)
-queue<int> q;
-deque<int> d; queue<int> q(d);
-array_list<int> l; queue<int> q(l);
-vector<int> v; queue<int> q(v);
-list<int> l; queue<int> q(l);
+  {
+    queue<int> q;
+  }
+  {
+    deque<int> d;
+    queue<int> q(d);
+  }
+  {
+    array_list<int> l;
+    queue<int> q(l);
+  }
+  {
+    vector<int> v;
+    queue<int> q(v);
+  }
+  {
+    list<int> l;
+    queue<int> q(l);
+  }
+}
